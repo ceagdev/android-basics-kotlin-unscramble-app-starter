54c0cf5ddf5882b3a081a0519b1781050060dd14
cesar acosta <ceagdev@gmail.com>
2021-07-16 18:42:51 -0400

    * ViewModel GameViewModel created * Expose private properties * Add viewModel life cycle Logs * Create instance of ViewModel in Fragment * Add Fragment life cycle Logs

---
+++ android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.android.unscramble.ui.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    private var _score = 0
+    val score : Int
+    get() = _score
+
+    private var _currentWordCount = 0
+    val currentWordCount : Int
+    get() = _currentWordCount
+
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+}
